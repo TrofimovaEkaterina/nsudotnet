b825fa24886d707dcbfca52d958aa1bacae31adf
Trofimova Ekaterina <onesen@yandex.ru>
2017-05-24 11:59:34 +0700

    Add files via upload
    Multiline comments processing added

--- LinesCounter/LinesCounter/Program.cs
+++ LinesCounter/LinesCounter/Program.cs
@@ -6,40 +6,84 @@ namespace LinesCounter
     {
         private static int CheckDir(String path, String fileExt)        //Проверка директории
         {
-         int linesCount = 0;
-         String[] directories = Directory.GetDirectories(path);      //Получение имен всех поддиректорий и их обход рекурсивно в глубину
+            
+            int linesCount = 0;
+            String[] directories = Directory.GetDirectories(path);      //Получение имен всех поддиректорий и их обход рекурсивно в глубину
             foreach (var dir in directories)
             {
                 linesCount += CheckDir(dir, fileExt);
             }
-           String[] files = Directory.GetFiles(path);                  //Получение имен всех файлов в директории и их обход
+            String[] files = Directory.GetFiles(path);                  //Получение имен всех файлов в директории и их обход
             foreach (var file in files)
             {
                 linesCount += CheckFile(file, fileExt);
             }
             return linesCount;
         }
+
         private static int CheckFile(String path, String fileExt)           //Проверка файла
         {
             int linesCount = 0;
+            bool inMultiComm = false;
+
             if (path.EndsWith(fileExt))                                     //Eсли файл обладает указанным расширением...
             {
-                StreamReader input = new StreamReader(path);                //Открытие потока на чтение
-                String str;
-                while ((str = input.ReadLine()) != null)
+                using (StreamReader input = new StreamReader(path))
                 {
-                    str = str.Replace(Environment.NewLine, "").Trim();                  //Удаление переносов строк
-                    if ((!str.StartsWith("//")) && (!str.Equals(String.Empty)))         //Строка не должна начинаться со слешей и не быть пустой
+                    String str;
+
+                    while ((str = input.ReadLine()) != null)
                     {
-                        linesCount++;
+                        str = str.Trim();
+
+                        if (str.Equals(String.Empty) || str.StartsWith("//"))
+                        {
+                            continue;
+                        }
+
+                        if (inMultiComm)
+                        {
+                            if (str.Contains("*/"))
+                            {
+                                str = str.Substring(str.LastIndexOf("*/") + 2);
+                                inMultiComm = false;
+                            }
+                            else {
+                                continue;
+                            }
+                        }
+
+                        while (str.Contains("/*"))
+                        {
+                            if (str.Contains("*/"))
+                            {
+                                str = str.Substring(str.LastIndexOf("*/") + 2);
+                            }
+                            else
+                            {
+                                inMultiComm = true;
+                                str = str.Substring(0, str.IndexOf("/*"));
+                                break;
+                            }
+                        }
+
+                        str = str.Trim();
+
+                        if (!str.Equals(String.Empty))
+                        {
+                            linesCount++;
+                            Console.WriteLine(str);
+                        }
                     }
                 }
             }
             return linesCount;
         }
+
+
         static void Main(string[] args)
         {
-            if (args.Length == 1)                                           //Проверка числа аргументов
+            if (args.Length == 1)
             {
                 String ext = Path.GetExtension(args[0]);
                 if (ext.Equals(System.String.Empty) || ext == null)
@@ -49,8 +93,8 @@ namespace LinesCounter
                 }
                 try
                 {
-                    String dir = Directory.GetCurrentDirectory();       //Получение текущуей директорию
-                    int linesCount = CheckDir(dir, ext);                //Запуск обхода
+                    String dir = Directory.GetCurrentDirectory();
+                    int linesCount = CheckDir(dir, ext);
                     Console.WriteLine(linesCount + " lines");
                 }
                 catch (Exception ex)
